e7d8d0eb78af3697367128f18063b03e374310c1
fionaeking <62763848+fionaeking@users.noreply.github.com>
2020-12-12 23:25:44 +0000

    Add Day4 Part1

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = File.ReadAllText("PuzzleInput.txt");
+            var input = a.Split("\r\n\r\n");
+            var passportFields = new List<string>() { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"};
+            var validPassports = input.Length;
+            Console.WriteLine($"The total number of passports is: {validPassports}");
+            foreach (var entry in input) 
+            {
+                var fields = entry
+                    .Replace("\n", " ")
+                    .Split(' ')
+                    .Select(x => x.Split(':')[0])
+                    .ToList();
+                if (MissingFields(fields, passportFields))
+                {
+                    validPassports--;
+                }
+            }
+            Console.WriteLine($"The number of valid passports is: {validPassports}");
+        }
+
+        public static bool MissingFields(List<string> a, List<string> b)
+        {
+            return b.Except(a).Any();
+        }
+    }
+}
