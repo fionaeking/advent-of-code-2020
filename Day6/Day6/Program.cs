5a713a5214b60cbf3bc7fe893bc8b828cf633524
fionaeking <62763848+fionaeking@users.noreply.github.com>
2020-12-13 22:35:02 +0000

    Add Day6 Part1

---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Read in values and split by newline
+            var input = File.ReadAllText("PuzzleInput.txt")
+                .Split("\r\n\r\n")
+                .ToList();
+            var sum = 0;
+            foreach (var x in input)
+            {
+                sum += x.Replace("\n", "")
+                    .Replace("\r", "")
+                    .Distinct()
+                    .Count();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
