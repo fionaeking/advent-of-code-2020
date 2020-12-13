afaf373a0be056fb5bd8e6f75ce73d6a5c4cc091
fionaeking <62763848+fionaeking@users.noreply.github.com>
2020-12-13 17:16:35 +0000

    Add Day4

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -7,31 +7,37 @@ namespace Day4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var a = File.ReadAllText("PuzzleInput.txt");
-            var input = a.Split("\r\n\r\n");
-            var passportFields = new List<string>() { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"};
+            var input = File.ReadAllText("PuzzleInput.txt").Split("\r\n\r\n");
+            var passportFields = new List<string> { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt" };
             var validPassports = input.Length;
             Console.WriteLine($"The total number of passports is: {validPassports}");
-            foreach (var entry in input) 
+            foreach (var entry in input)
             {
                 var fields = entry
-                    .Replace("\n", " ")
+                    .Replace("\n", " ").Replace("\r", "")
                     .Split(' ')
-                    .Select(x => x.Split(':')[0])
-                    .ToList();
-                if (MissingFields(fields, passportFields))
+                    .ToDictionary(x => x.Split(':')[0], x => x.Split(':')[1]);
+                if (MissingFromList(fields.Keys, passportFields))
                 {
                     validPassports--;
+                }
+                else
+                {
+                    var v = new Validation();
+                    if (!v.ValidateFields(fields))
+                    {
+                        validPassports--;
+                    }
                 }
             }
             Console.WriteLine($"The number of valid passports is: {validPassports}");
         }
 
-        public static bool MissingFields(List<string> a, List<string> b)
+        public static bool MissingFromList(IEnumerable<string> a, IEnumerable<string> b)
         {
             return b.Except(a).Any();
         }
-    }
+    }  
 }
---
+++ Day4/Day4/Validation.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Day4
+{
+    public class Validation
+    {
+        public bool ValidateFields(Dictionary<string, string> fields)
+        {
+            var passport = CreatePassportObject(fields);
+            return ValidateYear(passport.byr, 1920, 2002)
+            && ValidateYear(passport.iyr, 2010, 2020)
+            && ValidateYear(passport.eyr, 2020, 2030)
+            && ValidateHeight(passport.hgt)
+            && ValidateHairColour(passport.hcl)
+            && ValidateEyeColour(passport.ecl)
+            && ValidatePassportId(passport.pid);
+        }
+
+        private bool ValidateYear(string yearString, int minYear, int maxYear)
+        {
+            return int.TryParse(yearString, out var year) && (minYear <= year && year <= maxYear);
+        }
+
+        private bool ValidatePassportId(string pid)
+        {
+            // pid (Passport ID) - a nine-digit number, including leading zeroes.
+            var rgx = new Regex(@"^[0-9]{9}$");
+            return rgx.IsMatch(pid);
+        }
+
+        private bool ValidateEyeColour(string ecl)
+        {
+            // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+            var validEyeColours = new List<string>() { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+            return validEyeColours.Contains(ecl);
+        }
+
+        private bool ValidateHairColour(string hcl)
+        {
+            // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+            var rgx = new Regex(@"#([0-9]|[a-f]){6}");
+            return rgx.IsMatch(hcl);
+        }
+
+        private bool ValidateHeight(string hgt)
+        {
+            //hgt(Height) - a number followed by either cm or in:
+            //If cm, the number must be at least 150 and at most 193.
+            //If in, the number must be at least 59 and at most 76.
+            var unit = hgt.Substring(Math.Max(0, hgt.Length - 2));
+            var success = int.TryParse(hgt.Remove(Math.Max(0, hgt.Length - 2)), out var value);
+            if (!success)
+            {
+                return false;
+            }
+            else return ((unit == "cm" && 150 <= value && value <= 193)
+                | (unit == "in" && 59 <= value && value <= 76));
+        }
+
+        private Passport CreatePassportObject(Dictionary<string, string> fields)
+        {
+            return new Passport()
+            {
+                byr = fields["byr"],
+                ecl = fields["ecl"],
+                eyr = fields["eyr"],
+                hcl = fields["hcl"],
+                hgt = fields["hgt"],
+                iyr = fields["iyr"],
+                pid = fields["pid"]
+            };
+        }
+
+    }
+
+    public class Passport
+    {
+        public string ecl;
+        public string pid;
+        public string eyr;
+        public string hcl;
+        public string byr;
+        public string iyr;
+        public string hgt;
+    }
+}
