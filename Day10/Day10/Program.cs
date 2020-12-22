048a6648f9fa31f01aa24d0fce48ceacb50ea62f
fionaeking <62763848+fionaeking@users.noreply.github.com>
2020-12-22 23:33:31 +0000

    Add very quick Day 11 part 1

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -9,19 +9,52 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            var input = GetInputAsListNums().OrderBy(x=>x).ToList();
+            var input = GetInputAsListNums().OrderBy(x => x).ToList();
             input.Add(input.Last() + 3);
             var differences = input.Zip(input.Skip(1), (x, y) => y - x);
             var numberOnes = differences.Where(x => x == 1).Count();
             var numberThrees = differences.Where(x => x == 3).Count();
             Console.WriteLine($"{numberOnes} differences of 1 jolt; {numberThrees} differences of 3");
             // What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
-            Console.WriteLine($"The answer is {numberOnes*numberThrees}");
+            Console.WriteLine($"The answer is {numberOnes * numberThrees}");
+            GetAllCombinations(input);
         }
 
         private static List<int> GetInputAsListNums()
         {
             return File.ReadLines("PuzzleInput.txt").Select(int.Parse).Append(0).ToList();
+        }
+
+        private static void GetAllCombinations(List<int> input)
+        {
+            var numberOnes = new List<int>();
+            var count = 0;
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                if ((input[i + 1] - input[i]) == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    if (--count >= 1)
+                    {
+                        numberOnes.Add(count);
+                    }
+                    count = 0;
+                }
+
+            }
+            
+            long total = 1;
+            // 1 jolt - 2 ways, 2 jolt - 4 ways, 3 jolt - 7 ways
+            var differentMethods = new int[] { 1, 2, 4, 7 };
+            foreach (var c in numberOnes)
+            {
+                total *= differentMethods[c];
+            }
+
+            Console.WriteLine($"The total number of combinations is {total}");
         }
     }
 }
---
+++ Day11/Day11/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("PuzzleInput.txt")
+                .ToList()
+                .Select(x => x.ToCharArray())
+                .ToArray();
+            var secondInput = input.Select(a => (char[])a.Clone()).ToArray();
+
+            while (true)
+            {
+                var changed = false;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    for (int j = 0; j < input[0].Length; j++)
+                    {
+                        if (input[i][j] == 'L' && CheckIsOccupied(input, i, j, '#') == 0)
+                        {
+                            secondInput[i][j] = '#';
+                            changed = true;
+                        }
+                        else if (input[i][j] == '#' && CheckIsOccupied(input, i, j, '#') >= 4)
+                        {
+                            secondInput[i][j] = 'L';
+                            changed = true;
+                        }
+                        else
+                        {
+                            secondInput[i][j] = input[i][j];
+                        }
+                    }
+                }
+
+                if (changed == true)
+                {
+                    input = secondInput.Select(a => (char[])a.Clone()).ToArray();
+                }
+                else
+                {
+                    var count = secondInput.SelectMany(x => x).ToArray().Where(x=> x=='#').Count();
+                    Console.WriteLine(count);
+                    break;
+                }
+            }
+
+               
+        }
+
+        static int CheckIsOccupied(char[][] input, int i, int j, char ch)
+        {
+            var count = 0;
+            var maxY = input.Length - 1;
+            var maxX = input[0].Length - 1;
+
+            if (i < maxY)
+            {
+                count += EqualsChar(input[i + 1][j], ch);
+            }
+            if (j < maxX)
+            {
+                count += EqualsChar(input[i][j + 1], ch);
+            }
+            if (i > 0)
+            {
+                count += EqualsChar(input[i - 1][j], ch);
+            }
+            if (j > 0)
+            {
+                count += EqualsChar(input[i][j - 1], ch);
+            }
+            if (i < maxY & j < maxX)
+            {
+                count += EqualsChar(input[i + 1][j + 1], ch);
+            }
+            if (i < maxY & j > 0)
+            {
+                count += EqualsChar(input[i + 1][j - 1], ch);
+            }
+            if (i > 0 & j < maxX)
+            {
+                count += EqualsChar(input[i - 1][j + 1], ch);
+            }
+            if (i > 0 & j > 0)
+            {
+                count += EqualsChar(input[i - 1][j - 1], ch);
+            }
+            return count;
+        }
+
+        static int EqualsChar(char toCompare, char ch)
+        {
+            return toCompare == ch ? 1 : 0;
+        }
+
+    }
+}
