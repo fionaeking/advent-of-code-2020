7f47406e3133906d7d31342bd61b8f16e0fc9fe0
fionaeking <62763848+fionaeking@users.noreply.github.com>
2020-12-23 00:04:23 +0000

    Add a very messy Day 11 Part 2

--- Day11/Day11/Program.cs
+++ Day11/Day11/Program.cs
@@ -6,6 +6,7 @@ namespace Day11
 {
     class Program
     {
+        // TODO - this code is horrible
         static void Main()
         {
             var input = File.ReadAllLines("PuzzleInput.txt")
@@ -21,12 +22,12 @@ namespace Day11
                 {
                     for (int j = 0; j < input[0].Length; j++)
                     {
-                        if (input[i][j] == 'L' && CheckIsOccupied(input, i, j, '#') == 0)
+                        if (input[i][j] == 'L' && PartTwo(input, i, j, '#') == 0)
                         {
                             secondInput[i][j] = '#';
                             changed = true;
                         }
-                        else if (input[i][j] == '#' && CheckIsOccupied(input, i, j, '#') >= 4)
+                        else if (input[i][j] == '#' && PartTwo(input, i, j, '#') >= 5)
                         {
                             secondInput[i][j] = 'L';
                             changed = true;
@@ -48,9 +49,146 @@ namespace Day11
                     Console.WriteLine(count);
                     break;
                 }
+            }  
+            
+        }
+
+        static int PartTwo(char[][] input, int i, int j, char c)
+        {
+            var count = 0;
+            var maxY = input.Length - 1;
+            var maxX = input[0].Length - 1;
+            var jCopy = j;
+            var iCopy = i;
+
+            // check if j == maxX
+            if (j < maxX)
+            {
+                var check = false;
+                jCopy = j;
+                while (!check)
+                {
+                    check = CheckChar(input[i][++jCopy]);
+                    if (jCopy == maxX)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[i][jCopy], '#');
             }
 
-               
+            // check if j == 0
+            if (j > 0)
+            {
+                var check = false;
+                jCopy = j;
+                while (!check)
+                {
+                    check = CheckChar(input[i][--jCopy]);
+                    if(jCopy==0)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[i][jCopy], '#');
+            }
+
+            // check if i == 0
+            if (i > 0)
+            {
+                var check = false;
+                iCopy = i;
+                while (!check)
+                {
+                    check = CheckChar(input[--iCopy][j]);
+                    if (iCopy == 0)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[iCopy][j], '#');
+            }
+
+            // check if i == 0
+            if (i < maxY)
+            {
+                var check = false;
+                iCopy = i;
+                while (!check)
+                {
+                    check = CheckChar(input[++iCopy][j]);
+                    if (iCopy == maxY)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[iCopy][j], '#');
+            }
+
+            if (i < maxY & j < maxX)
+            {
+                var check = false;
+                iCopy = i;
+                jCopy = j;
+                while (!check)
+                {
+                    check = CheckChar(input[++iCopy][++jCopy]);
+                    if (iCopy == maxY | jCopy == maxX)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[iCopy][jCopy], '#');
+            }
+
+            if (i < maxY & j > 0)
+            {
+                var check = false;
+                iCopy = i;
+                jCopy = j;
+                while (!check)
+                {
+                    check = CheckChar(input[++iCopy][--jCopy]);
+                    if (iCopy == maxY | jCopy == 0)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[iCopy][jCopy], '#');
+            }
+
+            if (i > 0 & j > 0)
+            {
+                var check = false;
+                iCopy = i;
+                jCopy = j;
+                while (!check)
+                {
+                    check = CheckChar(input[--iCopy][--jCopy]);
+                    if (iCopy == 0 | jCopy == 0)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[iCopy][jCopy], '#');
+            }
+
+            if (i > 0 & j < maxX)
+            {
+                var check = false;
+                iCopy = i;
+                jCopy = j;
+                while (!check)
+                {
+                    check = CheckChar(input[--iCopy][++jCopy]);
+                    if (iCopy == 0 | jCopy == maxX)
+                    {
+                        break;
+                    }
+                }
+                count += EqualsChar(input[iCopy][jCopy], '#');
+            }
+            return count;
         }
 
         static int CheckIsOccupied(char[][] input, int i, int j, char ch)
@@ -59,37 +197,37 @@ namespace Day11
             var maxY = input.Length - 1;
             var maxX = input[0].Length - 1;
 
-            if (i < maxY)
-            {
-                count += EqualsChar(input[i + 1][j], ch);
-            }
             if (j < maxX)
             {
                 count += EqualsChar(input[i][j + 1], ch);
-            }
-            if (i > 0)
-            {
-                count += EqualsChar(input[i - 1][j], ch);
             }
             if (j > 0)
             {
                 count += EqualsChar(input[i][j - 1], ch);
             }
-            if (i < maxY & j < maxX)
+            if (i < maxY)
             {
-                count += EqualsChar(input[i + 1][j + 1], ch);
+                count += EqualsChar(input[i + 1][j], ch);
+                if (j < maxX)
+                {
+                    count += EqualsChar(input[i + 1][j + 1], ch);
+                }
+                if (j > 0)
+                {
+                    count += EqualsChar(input[i + 1][j - 1], ch);
+                }
             }
-            if (i < maxY & j > 0)
+            if (i > 0)
             {
-                count += EqualsChar(input[i + 1][j - 1], ch);
-            }
-            if (i > 0 & j < maxX)
-            {
-                count += EqualsChar(input[i - 1][j + 1], ch);
-            }
-            if (i > 0 & j > 0)
-            {
-                count += EqualsChar(input[i - 1][j - 1], ch);
+                count += EqualsChar(input[i - 1][j], ch);
+                if (j < maxX)
+                {
+                    count += EqualsChar(input[i - 1][j + 1], ch);
+                }
+                if (j > 0)
+                {
+                    count += EqualsChar(input[i - 1][j - 1], ch);
+                }
             }
             return count;
         }
@@ -97,6 +235,11 @@ namespace Day11
         static int EqualsChar(char toCompare, char ch)
         {
             return toCompare == ch ? 1 : 0;
+        }
+
+        static bool CheckChar(char ch)
+        {
+            return (ch == '#' | ch == 'L');
         }
 
     }
