5fdf330a71d4fd21fb6d6afefe873a0a3c8538a9
fionaeking <62763848+fionaeking@users.noreply.github.com>
2021-01-03 15:58:17 +0000

    Add Day12 Part2

--- Day12/Day12/Program.cs
+++ Day12/Day12/Program.cs
@@ -11,7 +11,12 @@ namespace Day12
             var input = File.ReadAllLines("PuzzleInput.txt").Select(x => x.ToCharArray());
             var currPointX = 0;
             var currPointY = 0;
-            var currDir = 90;  // facing 90 degrees (East)
+            // var currDir = 90;  // facing 90 degrees (East)
+            var currWaypointX = 10;
+            var currWaypointY = 1;
+            var nextWaypointX = 0;
+            var nextWaypointY = 0;
+            var iterations = 0;
             foreach (var instruction in input)
             {
                 var direction = instruction[0];
@@ -26,7 +31,7 @@ namespace Day12
                 Action R means to turn right the given number of degrees.
                 Action F means to move forward by the given value in the direction the ship is currently facing.
                 */
-                if (direction == 'F')
+                /*if (direction == 'F')
                 {
                     if (currDir == 0)
                     {
@@ -48,34 +53,68 @@ namespace Day12
                     {
                         Console.WriteLine("Unknown number of degrees");
                     }
-                }
+                }*/
                 switch(direction)
                 {
                     case 'N':
-                        currPointY += magnitude;
+                        // way point is x y
+                        currWaypointY += magnitude;
                         break;
                     case 'S':
-                        currPointY -= magnitude;
+                        currWaypointY -= magnitude;
                         break;
                     case 'E':
-                        currPointX += magnitude;
+                        currWaypointX += magnitude;
                         break;
                     case 'W':
-                        currPointX -= magnitude;
+                        currWaypointX -= magnitude;
                         break;
                     case 'L':
-                        currDir -= magnitude;
-                        currDir += 360;
-                        currDir = currDir % 360;
+                        iterations = magnitude / 90;
+                        while (iterations > 0)
+                        {
+                            nextWaypointX = currPointX + currPointY - currWaypointY;
+                            currWaypointY = currWaypointX + currPointY - currPointX;
+                            currWaypointX = nextWaypointX;
+                            iterations--;
+                        }
+                        //currDir -= magnitude;
+                        //currDir += 360;
+                        //currDir = currDir % 360;
                         break;
                     case 'R':
-                        currDir += magnitude;
-                        currDir = currDir % 360;
+                        iterations = magnitude / 90;
+                        while (iterations > 0)
+                        {
+                            nextWaypointX = currWaypointY - currPointY + currPointX;
+                            nextWaypointY = -currWaypointX + currPointX + currPointY;
+                            currWaypointX = nextWaypointX;
+                            currWaypointY = nextWaypointY;
+                            iterations--;
+                        }
+                        //currDir += magnitude;
+                        //currDir = currDir % 360;
+                        break;
+                    case 'F':
+                        //nextWaypointX = currWaypointX;
+                        //nextWaypointY = currWaypointY;
+                        var amountX = magnitude * (currWaypointX - currPointX);
+                        var amountY = magnitude * (currWaypointY - currPointY);
+                        currPointX += amountX;
+                        currPointY += amountY;
+                        //11670
+                        //10680
+                        //currWaypointX = currPointX + currWaypointX;
+                        //currWaypointY = currPointY + currWaypointY;
+                        currWaypointX += amountX;
+                        currWaypointY += amountY;
                         break;
                     default:
                         Console.WriteLine("Unknown direction");
                         break;
                 }
+                Console.WriteLine($"{currPointX} + {currPointY}");
+                Console.WriteLine($"{currWaypointX} + {currWaypointY}");
             }
             Console.WriteLine(Math.Abs(currPointX) + Math.Abs(currPointY));
         }
