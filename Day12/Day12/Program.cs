e204292eacf976c05da0841d0af21ce6c2f529c1
fionaeking <62763848+fionaeking@users.noreply.github.com>
2021-01-03 19:38:22 +0000

    Refactor a little

--- Day12/Day12/Program.cs
+++ Day12/Day12/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,45 +10,25 @@ namespace Day12
         static void Main(string[] args)
         {
             var input = File.ReadAllLines("PuzzleInput.txt").Select(x => x.ToCharArray());
+            Console.WriteLine(CalculateManhattanDistance(input));
+        }
+
+        private static int CalculateManhattanDistance(IEnumerable<char[]> input)
+        {
             var currPointX = 0;
             var currPointY = 0;
-            // var currDir = 90;  // facing 90 degrees (East)
             var currWaypointX = 10;
             var currWaypointY = 1;
-            var nextWaypointX = 0;
-            var nextWaypointY = 0;
-            var iterations = 0;
             foreach (var instruction in input)
             {
+                var nextWaypointX = 0;
+                var nextWaypointY = 0;
+                var iterations = 0;
                 var direction = instruction[0];
-                var magnitude = int.Parse(string.Join("",instruction.Skip(1)));
-                /*if (direction == 'F')
-                {
-                    if (currDir == 0)
-                    {
-                        direction = 'N';
-                    }
-                    else if (currDir == 90)
-                    {
-                        direction = 'E';
-                    }
-                    else if (currDir == 180)
-                    {
-                        direction = 'S';
-                    }
-                    else if (currDir == 270)
-                    {
-                        direction = 'W';
-                    }
-                    else
-                    {
-                        Console.WriteLine("Unknown number of degrees");
-                    }
-                }*/
-                switch(direction)
+                var magnitude = int.Parse(string.Join("", instruction.Skip(1)));
+                switch (direction)
                 {
                     case 'N':
-                        // way point is x y
                         currWaypointY += magnitude;
                         break;
                     case 'S':
@@ -68,9 +49,6 @@ namespace Day12
                             currWaypointX = nextWaypointX;
                             iterations--;
                         }
-                        //currDir -= magnitude;
-                        //currDir += 360;
-                        //currDir = currDir % 360;
                         break;
                     case 'R':
                         iterations = magnitude / 90;
@@ -82,8 +60,6 @@ namespace Day12
                             currWaypointY = nextWaypointY;
                             iterations--;
                         }
-                        //currDir += magnitude;
-                        //currDir = currDir % 360;
                         break;
                     case 'F':
                         nextWaypointX = currWaypointX + magnitude * (currWaypointX - currPointX);
@@ -98,7 +74,7 @@ namespace Day12
                         break;
                 }
             }
-            Console.WriteLine(Math.Abs(currPointX) + Math.Abs(currPointY));
+            return Math.Abs(currPointX) + Math.Abs(currPointY);
         }
     }
 }
