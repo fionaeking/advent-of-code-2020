530f9163b161491961e088b5ba0da3eecf92176b
fionaeking <62763848+fionaeking@users.noreply.github.com>
2021-01-03 19:55:49 +0000

    Add Day13 Part 1

---
+++ Day13/Day13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("PuzzleInput.txt");
+            var initialTimestamp = int.Parse(input[0]);
+            var timestamp = initialTimestamp - 1;  // account for initial loop
+            var buses = input[1].Split(',').Where(x => x != "x").Select(s => int.Parse(s));
+            var earliestBus = new List<int>();
+            while(earliestBus.Count<1)
+            {
+                timestamp++;
+                earliestBus.AddRange(buses.Where(x => CheckIfMultiple(x, timestamp) == true));
+            }
+            var busId = earliestBus.First();
+            var minsToWait = timestamp - initialTimestamp;
+            Console.WriteLine(busId * minsToWait);
+        }
+
+        private static bool CheckIfMultiple(int x, int timestamp)
+        {
+            return timestamp % x == 0;
+        }
+    }
+}
